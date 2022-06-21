3b2478c510ebe644a2899521a4475e1c0644b392
Doug Marttila <dmarttila@gmail.com>
2022-06-21 07:40:24 -0400

    Initial Commit

---
+++ WarmingStripes/WarmingStripesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmingStripesApp.swift
+//  WarmingStripes
+//
+//  Created by Douglas Marttila on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WarmingStripesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
