b5fcfb2dc9bd770695e003a4573c3944ca272b3c
Doug Marttila <dmarttila@gmail.com>
2022-06-25 20:42:49 -0400

    first pass at color scaling. Doesn't work, but moving in the right direction

--- WarmingStripes/model/Model.swift
+++ WarmingStripes/model/Model.swift
@@ -13,6 +13,9 @@ final class Model: ObservableObject{
 
     private let fileName = "HadCRUT.5.0.1.0.summary_series.global.annual"
 
+    var maxAnomaly: Double = 0
+    var minAnomaly: Double = 0
+
     func loadData () ->  [TemperatureAnomaly] {
         var anomalies: [TemperatureAnomaly] = []
         if let filepath = Bundle.main.path(forResource: fileName, ofType: "csv") {
@@ -23,6 +26,8 @@ final class Model: ObservableObject{
                     let values = datum.components(separatedBy: ",")
                     if let year = Int(values[0]), let tempDiff = Double(values[1]) {
                         anomalies.append(TemperatureAnomaly(year: year - 1849, anomaly: tempDiff))
+                        TemperatureAnomaly.minAnomaly = min(TemperatureAnomaly.minAnomaly, tempDiff)
+                        TemperatureAnomaly.maxAnomaly = max(TemperatureAnomaly.maxAnomaly, tempDiff)
                     }
                 }
             } catch {
@@ -36,13 +41,23 @@ final class Model: ObservableObject{
 
     init () {
         anomalies = loadData()
+
     }
 }
 
 struct TemperatureAnomaly: Identifiable {
+
+    static var maxAnomaly: Double = 0
+    static var minAnomaly: Double = 0
+
     //TODO: Change to dates rather than int
     var color: Color {
-        anomaly > 0 ? .red : .blue
+//        anomaly > 0 ? .red : .blue
+        if anomaly > 0 {
+            return Color(red: anomaly/TemperatureAnomaly.maxAnomaly, green: 0, blue: 0)
+        }
+        return Color(red:0, green: 0, blue: anomaly/TemperatureAnomaly.minAnomaly)
+        //let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
     }
     let year: Int
     let anomaly: Double
