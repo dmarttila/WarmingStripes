5df9f5d9ab4759ace8b27ec309fa9a400ad59d55
Doug Marttila <dmarttila@gmail.com>
2022-06-26 19:54:46 -0400

    added a toggle to show other (unnecessary) chart marks

--- WarmingStripes/ContentView.swift
+++ WarmingStripes/ContentView.swift
@@ -5,38 +5,62 @@
 //  Created by Douglas Marttila on 6/21/22.
 //
 
+/*TODO:
+ Keep or remove @envirnment
+ Better color range
+ Zoom in and out
+ Dupe all the other views
+ Set the axis scale
+ Get the bars touching and no stroke
+
+ */
+
 import SwiftUI
 import Charts
 
 struct ContentView: View {
-// environment object doesn't work in previews
-//    @EnvironmentObject var model: Model
-let model = Model()
-
+    // environment object doesn't work in previews
+    //    @EnvironmentObject var model: Model
+    let model = Model()
+    @State var showOtherMarks: Bool = false
     init() {
         //        let model = Model()
     }
 
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Toggle("Show other marts", isOn: $showOtherMarks)
             Chart (model.anomalies) { year in
                 BarMark(
                     x: .value("date", year.date, unit: .year),
                     y: .value("Total Count", year.anomaly)
                 )
                 .foregroundStyle(year.color)
-                //                    .foregroundStyle(<#T##style: ShapeStyle##ShapeStyle#>)
-                //                    .foregroundStyle(by: .value("Shape Color", shape.color))
-            }
-//            .chartForegroundStyleScale([
-//                "Green": .green, "Purple": .purple, "Pink": .pink, "Yellow": .yellow
-//            ])
+                if showOtherMarks {
+                    LineMark(
+                        x: .value("date", year.date, unit: .year),
+                        y: .value("Total Count", year.anomaly)
+                    )
+                    AreaMark(
+                        x: .value("date", year.date, unit: .year),
+                        y: .value("Total Count", year.anomaly)
+                    )
+                    .foregroundStyle(year.color)
+                    RectangleMark (
+                        x: .value("date", year.date, unit: .year),
+                        y: .value("Total Count", year.anomaly)
+                    )
+                    .foregroundStyle(year.color)
+                }
+
+            //                    .foregroundStyle(<#T##style: ShapeStyle##ShapeStyle#>)
+            //                    .foregroundStyle(by: .value("Shape Color", shape.color))
         }
+        //            .chartForegroundStyleScale([
+        //                "Green": .green, "Purple": .purple, "Pink": .pink, "Yellow": .yellow
+        //            ])
     }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
--- WarmingStripes/WarmingStripesApp.swift
+++ WarmingStripes/WarmingStripesApp.swift
@@ -14,6 +14,7 @@ struct WarmingStripesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(model)
+                .preferredColorScheme(.dark)
         }
     }
 }
